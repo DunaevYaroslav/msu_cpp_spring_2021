ddcbae1ac45f8f15f9605e40ed351cf9d77ecde1
DunaevYaroslav <70894113+DunaevYaroslav@users.noreply.github.com>
2021-05-03 22:43:09 +0300

    Add files via upload

--- 03/main.cpp
+++ 03/main.cpp
@@ -28,10 +28,100 @@ void Test2(){
     int a = m1[0][0];
     assert(a == 5);
 }
+void Test3()
+{
+    Matrix m(5, 9);
+    assert(m.getRows() == 5);
+    assert(m.getColumns() == 9);
+    m[4][4] = 5; // строка 1, колонка 2
 
+    double x = m[4][4];
+    assert(x == 5);
+    m *= 3; // умножение на число
+    double t = m[4][4];
+    assert(t==15);
+}
+void Test4()
+{
+    Matrix m1(5, 5);
+    Matrix m2(5, 5);
+    assert(m1 == m2);
+    m1[0][0] = 5;
+    assert(m1 != m2);
+}
+void Test5()
+{
+    Matrix m1(1, 1);
+    Matrix m2(1, 1);
+    assert(m1 == m2);
+    m2 = m1 + m2;
+    assert(m1 != m2);
+}
+void Test6()
+{
+    Matrix m(2, 2);
+    m[1][1] = 1; 
+    m[1][0] = 2;
+    m[0][1] = 3;
+    m[0][0] = 4; 
+
+    assert(m[1][0] == 2);
+    assert(m[0][1] == 3);
+}
+void Test7(){
+    Matrix m1(5, 2);
+    Matrix m2(2, 5);
+    assert(m1 != m2);
+}
+void Test8(){
+    Matrix m(5, 2);
+    bool b = false;
+    try
+    {
+        m[10][2] = 6;
+    }
+    catch(...)
+    {
+        b = true;
+    }
+    assert(b);
+}
+void Test9(){
+    Matrix m(2, 2);
+    m[0][0] = 1;
+    m[0][1] = 2;
+    m[1][0] = 4;
+    m[1][1] = 5;
+    m *= 5;
+    assert(m[0][0] == 5);
+    assert(m[0][1] == 10);
+    assert(m[1][0] == 20);
+    assert(m[1][1] == 25);
+}
+void Test10(){
+    Matrix m1(5, 2);
+    Matrix m2(2, 5);
+    bool b = false;
+    try
+    {
+        m1 = m1 + m2;
+    }
+    catch(...){
+        b = true;
+    }
+    assert(b);
+}
 int main() {
     Test1();
     Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+    Test8();
+    Test9();
+    Test10();
     std::cout<<"Success";
     return 0;
 }
