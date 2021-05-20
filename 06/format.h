f8a769f5a91f14764010245175d9bf8c465a42a0
DunaevYaroslav <70894113+DunaevYaroslav@users.noreply.github.com>
2021-05-20 16:42:47 +0300

    Add files via upload

--- 06/format.h
+++ 06/format.h
@@ -4,21 +4,23 @@
 #include <sstream>
 #include <iostream>
 
-bool try_digit(std::string str) {
-  for (uint64_t i = 0; i < str.size(); i++) {
-    if (!isdigit(str[i]))
-      return false;
-  }
-  return true;
+bool try_digit(std::string str)
+{
+    for (uint64_t i = 0; i < str.size(); i++)
+    {
+        if (!isdigit(str[i]))
+            return false;
+    }
+    return true;
 }
 
-class format_exc: std::exception
+class format_exc : std::exception
 {
 private:
     std::string what_;
 
 public:
-    explicit format_exc(std::string what): what_(std::move(what))
+    explicit format_exc(std::string what) : what_(std::move(what))
     {
     }
 
@@ -31,9 +33,12 @@ public:
 int get_num(const std::string &s)
 {
     int res;
-    try {
+    try
+    {
         res = std::stoi(s);
-    } catch (const std::logic_error&) {
+    }
+    catch (const std::logic_error &)
+    {
         throw format_exc("Incorrect string format");
     }
     return res;
@@ -41,8 +46,10 @@ int get_num(const std::string &s)
 
 std::string res_check(const std::string &s)
 {
-    for (auto c: s) {
-        if (c == '{' || c == '}') {
+    for (auto c : s)
+    {
+        if (c == '{' || c == '}')
+        {
             throw format_exc("Not enough arguments");
         }
     }
@@ -58,43 +65,60 @@ std::string process(const std::string &s, T arg, int n)
     size_t end = std::string::npos;
     std::string cur_str{};
     std::ostringstream res{};
-    while (i < s.length()) {
-    begin = s.find_first_of("{", i);
-    end = s.find_first_of("}", i);
-    if (begin > end) {
-      throw format_exc("expected { before }");
-    }
-    if (end == std::string::npos && begin != std::string::npos) {
-      throw format_exc("expected } after {");
-    }
-        if (s[i] == '{') {
+    while (i < s.length())
+    {
+        begin = s.find_first_of("{", i);
+        end = s.find_first_of("}", i);
+        if (begin > end)
+        {
+            throw format_exc("expected { before }");
+        }
+        if (end == std::string::npos && begin != std::string::npos)
+        {
+            throw format_exc("expected } after {");
+        }
+        if (s[i] == '{')
+        {
             i++;
             cur_str = "";
-            while (i < s.length() && s[i] != '}') {
+            while (i < s.length() && s[i] != '}')
+            {
                 cur_str += s[i];
                 i++;
             }
-            if (n == get_num(cur_str)) {
+            if (n == get_num(cur_str))
+            {
                 res << arg;
                 find = true;
-            } else {
+            }
+            else
+            {
                 res << ("{" + cur_str + "}");
             }
-        } else {
+        }
+        else
+        {
             res << s[i];
         }
         i++;
     }
-    if (try_digit(cur_str) && cur_str != "") {
-      try {
-        int number = std::stoi(cur_str);
-      } catch (const std::out_of_range &err) {
-        throw format_exc("incorrect input");
-      }
-    } else {
-      throw format_exc("incorrect input");
+    if (try_digit(cur_str) && cur_str != "")
+    {
+        try
+        {
+            int number = std::stoi(cur_str);
+        }
+        catch (const std::out_of_range &err)
+        {
+            throw format_exc("incorrect input");
+        }
     }
-    if (!find) {
+    else
+    {
+        throw format_exc("incorrect input");
+    }
+    if (!find)
+    {
         throw format_exc("Incorrect string format");
     }
     return res.str();
@@ -104,10 +128,13 @@ int counter(bool f = false)
 {
     static int n = -1;
     int cur = n;
-    if (f) {
+    if (f)
+    {
         n = -1;
         return cur + 1;
-    } else {
+    }
+    else
+    {
         n++;
         cur = n;
         return cur;
@@ -120,13 +147,21 @@ std::string format(const std::string &s, T arg)
     return res_check(process(s, arg, counter(true)));
 }
 
+template <class... Args>
+std::string format(const std::string &s, const Args &...args)
+{
+    return s;
+}
 template <typename T, typename... ArgsT>
 std::string format(const std::string &s, T arg, ArgsT... args)
 {
     std::string res;
-    try {
+    try
+    {
         res = process(s, arg, counter());
-    } catch (const format_exc&) {
+    }
+    catch (const format_exc &)
+    {
         counter(true);
     }
     return format(res, std::forward<ArgsT>(args)...);
--- 06/main.cpp
+++ 06/main.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <cassert>
 
-void TestCorrectSimpleWork1(){
+void TestCorrectSimpleWork1()
+{
     std::string f1 = "{0} + {1} = {2}";
     assert(format(f1, 5, 6, 11) == "5 + 6 = 11");
 }
@@ -18,82 +19,186 @@ void TestWithoutSeparator()
     assert(f2 == "100 = 100");
 }
 
-void TestExtraBracket() 
+void TestExtraBracket()
 {
- try {
-    auto text = format("{1}+{{1} = {0}", 2, "one");
-  } catch (const format_exc &err) {
-    std::string catch_error("incorrect input");
-    assert(err.what() == catch_error);
-  }
+    try
+    {
+        auto text = format("{1}+{{1} = {0}", 2, "one");
+    }
+    catch (const format_exc &err)
+    {
+        std::string catch_error("incorrect input");
+        assert(err.what() == catch_error);
+    }
 }
 
-void TestNotEnough(){
+void TestNotEnough()
+{
     std::string f1 = "{0} + {1} = {2}";
-    try {
+    try
+    {
         format(f1, 5, 6);
-    } catch (const format_exc &re) {
+    }
+    catch (const format_exc &re)
+    {
         std::string s = re.what();
         assert(s == "Not enough arguments");
     }
 }
 
-void TestTooMany(){
+void TestTooMany()
+{
     std::string f1 = "{0} + {1} = {2}";
-    try {
+    try
+    {
         format(f1, 5, 6, 11, 12);
-    } catch (const format_exc &re) {
+    }
+    catch (const format_exc &re)
+    {
         std::string s = re.what();
         assert(s == "incorrect input");
     }
 }
 
-void TestStringInsteadInt(){
+void TestStringInsteadInt()
+{
     std::string f1 = "{0} + {1} = {2}";
-    try {
+    try
+    {
         format("{error} {0} {1}", 5, 6, 7);
-    } catch (const format_exc &re) {
+    }
+    catch (const format_exc &re)
+    {
         std::string s = re.what();
         assert(s == "incorrect input");
     }
 }
 
-void TestStringWithInt(){
+void TestStringWithInt()
+{
     std::string f2 = "Today is {3}. Current time is {0}:{1}. Current year is {2}.";
     assert(format(f2, 19, 30, 2020, "Tuesday") == "Today is Tuesday. Current time is 19:30. Current year is 2020.");
 }
 
-void TestStringInsteadDouble(){
+void TestStringInsteadDouble()
+{
     std::string f3 = "{1} / {2} = {0}";
     assert(format(f3, "0.(3)", 1, 3) == "1 / 3 = 0.(3)");
 }
 
-void TestCopy(){
+void TestCopy()
+{
     std::ostringstream oss;
     oss << format("{1}{0}{0}", '+', 'C');
     assert(oss.str() == "C++");
 }
-void TestEmptyBrackets() 
+void TestEmptyBrackets()
 {
- try {
-    auto text = format("{1}+{} = {0}", 2, "one");
+    try
+    {
+        auto text = format("{1}+{} = {0}", 2, "one");
+    }
+    catch (const format_exc &err)
+    {
+        std::string catch_error("incorrect input");
+        assert(err.what() == catch_error);
+    }
+}
+
+void TestMixIntStr()
+{
+    try
+    {
+        auto text = format("{1}+{12one} = {0}", 2, "one");
+    }
+    catch (const format_exc &err)
+    {
+        std::string catch_error("incorrect input");
+        assert(err.what() == catch_error);
+    }
+}
+
+void TestEmpty()
+{
+    auto text = format("");
+    assert(text == "");
+}
+
+void TestWithoutFormating()
+{
+    auto text = format("qwerty");
+    assert(text == "qwerty");
+}
+
+void TestOneToken(){
+    auto text = format("{0}", "test");
+    assert(text == "test");
+} 
+
+void TestSeveralN(){
+    auto text = format("{0}{0}{0}", "test");
+    assert(text == "testtesttest");
+} 
+
+void TestFailStrAndInt(){
+    try
+    {
+        auto text = format("{1}+{1one} = {0}", 210, "one");
+    }
+    catch (const format_exc &err)
+    {
+        std::string catch_error("incorrect input");
+        assert(err.what() == catch_error);
+    }
+}
+
+void TestBigN1(){
+    try
+    {
+        auto text = format("{0} = {1}", "18446744073709551616", "two in 64");
+    }
+    catch (const format_exc &err)
+    {
+        std::string catch_error("incorrect input");
+        assert(err.what() == catch_error);
+    }
+}
+
+void TestBigN2(){
+    try
+    {
+        auto text = format("{18446744073709551616} = {1}", 10, "two in 64");
+    }
+    catch (const format_exc &err)
+    {
+        std::string catch_error("incorrect input");
+        assert(err.what() == catch_error);
+    }
+}
+
+void TestBrackets(){
+  try {
+    auto text = format("{}{}[}{}{}{} = {0}", 2, "one");
   } catch (const format_exc &err) {
     std::string catch_error("incorrect input");
     assert(err.what() == catch_error);
   }
-}
 
-void TestMixIntStr() 
-{
- try {
-    auto text = format("{1}+{12one} = {0}", 2, "one");
+  try {
+    auto text = format("{1}+{1}{ = {0}", 2, "one");
   } catch (const format_exc &err) {
     std::string catch_error("incorrect input");
     assert(err.what() == catch_error);
   }
+    try {
+    auto text = format("{0}{", 2);
+  } catch (const format_exc &err) {
+    std::string catch_error("expected } after {");
+    assert(err.what() == catch_error);
+  }
 }
-
-int main() {
+int main()
+{
     TestCorrectSimpleWork1();
     TestCorrectSimpleWork2();
     TestNotEnough();
@@ -105,6 +210,14 @@ int main() {
     TestExtraBracket();
     TestEmptyBrackets();
     TestMixIntStr();
+    TestEmpty();
+    TestWithoutFormating();
+    TestOneToken();
+    TestSeveralN();
+    TestFailStrAndInt();
+    TestBigN1();
+    TestBigN2();
+    TestBrackets();
     std::cout << "Success";
     return 0;
 }
